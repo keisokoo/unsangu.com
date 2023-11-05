07b875f382ae20a2c9e1f87beb24698c12a38c5a
keisokoo <keisokoo@gmail.com>
2023-11-05 17:36:09 +0900

    add, ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,14 +35,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-    <Script id="google-tag" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PFG3G4G8');`}</Script>
       <head>
-      {/* Start Google Tag Manager */}
-{/* End Google Tag Manager */}
+          {/* Start Google Tag Manager */}
+          <Script id="google-tag" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PFG3G4G8');`}</Script>
+        {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-LDDD08LDK3"/>
+        <Script id="google-analytics">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LDDD08LDK3');`}
+        </Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={clsx(noto.variable, roboto.variable, noto.className)}>
